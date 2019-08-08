b1c80e7810975828eda5ad20c86f1fee1bce08b3
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-08-08 13:32:23 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,63 @@
+<script>
+  import uuidv1 from 'uuid/v1'
+  import uuidv4 from 'uuid/v4'
+
+  let currUuid = uuidv1()
+  let optionsOpen = false
+  let selectedOption = 'v1'
+
+  function onClick() {
+    let newUuid = ''
+    if (selectedOption === 'v4') {
+      newUuid = uuidv4()
+    } else {
+      newUuid = uuidv1()
+    }
+
+    currUuid = newUuid
+  }
+
+  function handleRadio(e) {
+    selectedOption = e.target.value
+  }
+
+  const toggleOptions = () => optionsOpen = !optionsOpen
+</script>
+
+<div class="row">
+  <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Generate a uuid</h5>
+        <p class="card-text">
+          Simple UI to generate and display a uuid.
+          <button 
+            type="button" 
+            class="btn btn-outline-secondary" 
+            on:click={toggleOptions}
+          >
+            Options {#if !optionsOpen}+{:else}-{/if}
+          </button>
+        </p>
+        {#if optionsOpen}
+          <div class="form-group" on:change={handleRadio}>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="v1" value="v1" checked>
+              <label class="form-check-label" for="v1">v1</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="v4" value="v4">
+              <label class="form-check-label" for="v4">v4</label>
+            </div>
+          </div>
+        {/if}
+        <div class="form-group">
+          <input class="form-control" type="text" bind:value={currUuid}>
+        </div>
+        <button type="button" class="btn btn-primary btn-block" on:click={onClick}>Generate</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+

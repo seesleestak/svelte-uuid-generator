e74d65f2fbf6c2a30de6baf99bce2183b6620676
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-08-08 16:20:15 -0700

    Bind selectedOption to radio buttons

--- src/App.svelte
+++ src/App.svelte
@@ -21,43 +21,43 @@
     selectedOption = e.target.value
   }
 
-  const toggleOptions = () => optionsOpen = !optionsOpen
+  const toggleOptions = () => (optionsOpen = !optionsOpen)
 </script>
 
-<div class="row">
-  <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Generate a uuid</h5>
-        <p class="card-text">
-          Simple UI to generate and display a uuid.
-          <button 
-            type="button" 
-            class="btn btn-outline-secondary" 
-            on:click={toggleOptions}
-          >
-            Options {#if !optionsOpen}+{:else}-{/if}
-          </button>
-        </p>
-        {#if optionsOpen}
-          <div class="form-group" on:change={handleRadio}>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="v1" value="v1" checked>
-              <label class="form-check-label" for="v1">v1</label>
+<div class="p-0 m-4 container-fluid">
+  <div class="row">
+    <div class="col-12 col-sm-9 col-md-7 col-lg-6 col-xl-5">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Generate a uuid</h5>
+          <p class="card-text">
+            Simple UI to generate and display a uuid.
+            <button 
+              type="button" 
+              class="btn btn-outline-secondary" 
+              on:click={toggleOptions}
+            >
+              Options {#if !optionsOpen}+{:else}-{/if}
+            </button>
+          </p>
+          {#if optionsOpen}
+            <div class="form-group" on:change={handleRadio}>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="v1" value="v1" bind:group={selectedOption}>
+                <label class="form-check-label" for="v1">v1</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="v4" value="v4" bind:group={selectedOption}>
+                <label class="form-check-label" for="v4">v4</label>
+              </div>
             </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="v4" value="v4">
-              <label class="form-check-label" for="v4">v4</label>
-            </div>
+          {/if}
+          <div class="form-group">
+            <input class="form-control" type="text" bind:value={currUuid}>
           </div>
-        {/if}
-        <div class="form-group">
-          <input class="form-control" type="text" bind:value={currUuid}>
+          <button type="button" class="btn btn-primary btn-block" on:click={onClick}>Generate</button>
         </div>
-        <button type="button" class="btn btn-primary btn-block" on:click={onClick}>Generate</button>
       </div>
     </div>
   </div>
 </div>
-
-
